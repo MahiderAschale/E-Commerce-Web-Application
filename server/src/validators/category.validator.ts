955757f5f487483cdaf46d66dc37c5ce1add4186
MahiderAschale <mahideraschale816@gmail.com>
2026-08-12 19:35:16 +0300

    remove slug from catagories validator

--- server/src/validators/category.validator.ts
+++ server/src/validators/category.validator.ts
@@ -6,15 +6,7 @@ export const createCategorySchema = z.object({
     .min(2, "Category name must be at least 2 characters")
     .max(100),
 
-  slug: z
-    .string()
-    .min(2)
-    .max(100)
-    .regex(
-      /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
-      "Slug must contain only lowercase letters, numbers and hyphens"
-    ),
-
+  
   description: z
     .string()
     .max(500)
